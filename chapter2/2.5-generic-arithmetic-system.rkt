eb57d1f7fd81a91cf570f21da870cc2cff379c68
hashstone <hashstone@qq.com>
2024-12-19 22:20:48 +0800

    dev: complete 2.81 - 2.85

--- chapter2/2.5-generic-arithmetic-system.rkt
+++ chapter2/2.5-generic-arithmetic-system.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require racket/trace)
 (include "put-get.rkt")
 
 ; utils
@@ -8,7 +9,8 @@
       (cons type-tag contents)))
 
 (define (type-tag datum)
-  (cond ((number? datum) 'scheme-number)
+  (cond ((integer? datum) 'scheme-number) ; tricky, in scheme, (integer? 2.0) => #t, can't distingush its type
+        ((real? datum) 'scheme-real)
         ((pair? datum) (car datum))
         (else (error "Bad tagged datum -- TYPE-TAG" datum))))
 
@@ -19,17 +21,71 @@
 
 (define (square x) (* x x))
 
+; 2.83 - 2.84
+(define (install-type-level)
+  ; type level
+  (put 'scheme-number 'typelevel 0)
+  (put 'rational 'typelevel 1)
+  (put 'scheme-real 'typelevel 2)
+  (put 'complex 'typelevel 3)
+
+  'typelevel-done
+)
+(install-type-level)
+
+(define (compare t1 t2)
+  (- (get t1 'typelevel) (get t2 'typelevel)))
+
+; version 1
+;(define (apply-generic op . args)
+;  (let ((type-tags (map type-tag args)))
+;    (let ((proc (get op type-tags)))
+;      (if (not (null? proc))
+;          (apply proc (map contents args))
+;          (error "No method for these type -- APPLY-GENERIC" (list op type-tags)))
+;    )
+;  )
+;)
+
+; version 2
+;(define (apply-generic op . args)
+;  (let ((type-tags (map type-tag args)))
+;    (let ((proc (get op type-tags)))
+;      (if (not (null? proc))
+;          (apply proc (map contents args))
+;          (if (= (length args) 2)
+;              (let ((type1 (car type-tags))
+;                    (type2 (cadr type-tags))
+;                    (a1 (car args))
+;                    (a2 (cadr args)))
+;                (if (equal? type1 type2)
+;                    (error "No method for these types" (list op type-tags))
+;                    (let ((t1->t2 (get-coercion type1 type2))
+;                          (t2->t1 (get-coercion type2 type1)))
+;                      (cond ((not (null? t1->t2)) (apply-generic op (t1->t2 a1) a2))
+;                            ((not (null? t2->t1)) (apply-generic op a1 (t2->t1 a2)))
+;                            (else (error "No method for these types" (list op type-tags)))))))
+;              (error "No method for these types" (list op type-tags)))))))
+; 2.83-2.85
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
       (if (not (null? proc))
-          (apply proc (map contents args))
-          (error "No method for these type -- APPLY-GENERIC" (list op type-tags)))
-    )
-  )
-)
+          (drop (apply proc (map contents args)))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (if (equal? type1 type2)
+                    (error "No method for these types" (list op type-tags))
+                    (let ((flag (compare type1 type2)))
+                      (if (> flag 0)
+                          (apply-generic op a1 (raise a2))
+                          (apply-generic op (raise a1) a2)))))
+              (error "No method for these types" (list op type-tags)))))))
 
-; scheme-number
+; scheme-number stand for integer
 (define (install-scheme-number-package)
   (define (tag x)
     (attach-tag 'scheme-number x))
@@ -47,6 +103,36 @@
        (lambda (x y) (= x y)))
   (put '=zero? '(scheme-number)
        (lambda (x) (= x 0)))
+  (put 'exp '(scheme-number scheme-number)
+       (lambda (x y) (tag (expt x y)))) ; using primitive expt
+  (put 'raise '(scheme-number)
+       (lambda (x) (make-rational x 1)))
+  (put 'drop '(scheme-number) (lambda (x) (tag x)))
+  (put 'can-drop? '(scheme-number) (lambda (x) (#f)))
+  'done)
+
+; scheme-real stand for float
+(define (install-scheme-real-package)
+  (define (tag x)
+    (attach-tag 'scheme-real x))
+  (put 'add '(scheme-real scheme-real)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-real scheme-real)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-real scheme-real)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-real scheme-real)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-real
+       (lambda (x) (tag x)))
+  (put 'equ '(scheme-real scheme-real)
+       (lambda (x y) (= x y)))
+  (put '=zero? '(scheme-real)
+       (lambda (x) (= x 0.0)))
+  (put 'exp '(scheme-real scheme-real)
+       (lambda (x y) (tag (expt x y)))) ; using primitive expt
+  (put 'raise '(scheme-real)
+       (lambda (x) (make-complex-from-real-imag x 0)))
   'done)
 
 ; rational-number
@@ -88,7 +174,9 @@
   (put 'equ '(rational rational)
        (lambda (x y) (equ-rat x y)))
   (put '=zero? '(rational) =zero?)
-
+  (put 'raise '(rational)
+       (lambda (x) (make-scheme-real (/ (* 1.0 (numer x)) (denom x)))))
+  (put 'project '(rational) (lambda (x) (make-scheme-number (numer x))))
   'done)
 
 ; complex number
@@ -196,6 +284,7 @@
   (put 'equ '(complex complex)
        (lambda (z1 z2) (equ-complex z1 z2)))
   (put '=zero? '(complex) =zero?)
+  (put 'project '(complex) (lambda (z) (make-scheme-real (real-part z))))
 
   ; 注意 '(complex), 而不是 'complex，因为 apply-generic::(map type-tag args) 返回的是list
   (put 'imag-part '(complex) imag-part)
@@ -204,10 +293,13 @@
   (put 'magnitude '(complex) magnitude)
   'done)
 
+
 ; install packages
 (install-scheme-number-package)
 (install-rational-package)
+(install-scheme-real-package)
 (install-complex-package)
+(install-type-level)
 
 ; package export API
 (define (make-scheme-number n)
@@ -216,19 +308,55 @@
 (define (make-rational n d)
   ((get 'make 'rational) n d))
 
+(define (make-scheme-real f)
+  ((get 'make 'scheme-real) f))
+
 (define (make-complex-from-real-imag x y)
   ((get 'make-from-real-imag 'complex) x y))
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
-
 
 ; generic calc system
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
+(define (exp x y) (apply-generic 'exp x y)) ; 2.81 a)
+
+(define (raise x)
+  (let ((raise-proc (get 'raise (list (type-tag x)))))
+    (if raise-proc
+        (raise-proc (contents x))
+        #f)))
+
+(define (project x)
+  (let ((proc (get 'project (list (type-tag x)))))
+    (if proc
+        (proc (contents x))
+        #f)))
+
+(define (equ?-print x y)
+  (display x) (display " = ") (display y) (display ":") (display (equ? x y))
+  (apply-generic 'equ x y)
+  (newline)
+)
+
+(define (equ? x y)
+  (if (eq? (type-tag x) (type-tag y))
+      (apply-generic 'equ x y)
+      #f))
+
+(define (drop x)
+  (if (pair? x) ; 过滤 #t、#f 等没有 type-tag 的参数
+      (let ((x-project (project x)))
+        (if (and x-project
+                 (equ? (raise x-project) x))
+            (drop x-project)
+            x))
+      x))
 
 ; test cases
+;(trace apply-generic)
 (add (make-scheme-number 3) (make-scheme-number 4))
 (sub (make-scheme-number 3) (make-scheme-number 4))
 (mul (make-scheme-number 3) (make-scheme-number 4))
@@ -258,16 +386,6 @@
 
 ; 2.79
 (display "\n====> test 2.79\n")
-(define (equ?-print x y)
-  (display x) (display " = ") (display y) (display ":") (display (equ? x y))
-  (apply-generic 'equ x y)
-  (newline)
-)
-(define (equ? x y)
-  (if (eq? (type-tag x) (type-tag y))
-      (apply-generic 'equ x y)
-      (error "not the same tag-type" x y)))
-
 (equ?-print 5 (make-scheme-number 5))
 (equ?-print (make-scheme-number 3) (make-scheme-number 4))
 (equ?-print (make-scheme-number 3) (make-scheme-number 3))
@@ -296,3 +414,60 @@
 (=zero? (make-complex-from-real-imag 0 0))
 (=zero? (make-complex-from-real-imag 3 0))
 (=zero? (make-complex-from-real-imag 0 3))
+
+
+;; type-transform, type-[force, raise(promotion), demotion]
+;; type force
+(newline)
+
+(#%require (only racket/base make-hash))
+(#%require (only racket/base hash-set!))
+(#%require (only racket/base hash-ref))
+
+(define *coercion-table* (make-hash))
+
+(define (put-coercion op type proc)
+  (hash-set! *coercion-table* (list op type) proc)
+)
+
+(define (get-coercion op type)
+  (hash-ref *coercion-table* (list op type) '())
+)
+
+(define (scheme-number->complex n) (make-complex-from-real-imag (contents n) 0))
+;(define (scheme-number->scheme-number n) n)
+;(define (complex->complex x) x)
+
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+;(put-coercion 'scheme-number 'scheme-number scheme-number->scheme-number)
+;(put-coercion 'complex 'complex complex->complex)
+
+;(trace apply-generic)
+(exp 5 3)
+;(exp (make-complex-from-real-imag 3 1) (make-complex-from-real-imag 5 2))
+
+; 2.82
+; 寻找公共父节点
+
+; 2.83 - 2.84
+(newline)
+(display "\n====> test 2.83 - 2.85\n")
+(add 3 (make-rational 2 1))
+(sub (make-rational 3 2) 5)
+(add (make-rational 3 9) (make-scheme-real 1.5))
+(div (make-scheme-real 3.0) 2)
+
+;(trace apply-generic)
+;(trace drop)
+;(trace raise)
+;(trace project)
+; TODO fix (add 2 (make-scheme-real 1.33)) problem
+(add 1.9 (make-scheme-real 1.3333))
+(add 1.2 (make-complex-from-real-imag 3 5))
+
+; 2.85
+(newline)
+(add (make-complex-from-real-imag 2 -5) (make-complex-from-real-imag 3 5))
+(add (make-complex-from-real-imag 2 -5.5) (make-complex-from-real-imag 3.2 5.5))
+
+(div (make-rational 4 5) (make-rational 2 5))
